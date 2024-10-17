67c198c0b53e4bc70ceb90a98f629ff648ed89ba
RLeandro05 <lligpic577@g.educaand.es>
2024-10-17 10:35:26 +0200

    Añadido Ejercicio 4 sin terminar aún. PENDIENTE POR TERMINAR

---
+++ Ejercicio4/Ejercicio4.php
@@ -0,0 +1,87 @@
+<?php 
+    // Ejercicio 4
+
+    echo nl2br("<h2>Tirada de 6 dados</h2>\n");
+
+    $listaDados = [];
+    for ($i = 1; $i<= 6; $i++) {
+        $dadoAleatorio = rand(1, 6);
+        switch ($dadoAleatorio) { // Hacer un switch y mostrar cara del dado correspondiente con el número aleatorio
+            case 1:
+                echo "<img src=\"../img/1.svg\"/>";
+                $listaDados [] = $dadoAleatorio;
+                break;
+            case 2:
+                echo "<img src=\"../img/2.svg\"/>";
+                $listaDados [] = $dadoAleatorio;
+                break;
+            case 3:
+                echo "<img src=\"../img/3.svg\"/>";
+                $listaDados [] = $dadoAleatorio;
+                break;
+            case 4:
+                echo "<img src=\"../img/4.svg\"/>";
+                $listaDados [] = $dadoAleatorio;
+                break;
+            case 5:
+                echo "<img src=\"../img/5.svg\"/>";
+                $listaDados [] = $dadoAleatorio;
+                break;
+            case 6:
+                echo "<img src=\"../img/6.svg\"/>";
+                $listaDados [] = $dadoAleatorio;
+                break;
+         }
+    }
+
+    echo nl2br("<h2>Dado a eliminar</h2>\n");
+
+    $dadoaEliminar = rand(1, 6);
+    switch ($dadoaEliminar) { // Eliminar el dado que salga aleatoriamente
+        case 1:
+            echo "<img src=\"../img/1.svg\"/>";
+            break;
+        case 2:
+            echo "<img src=\"../img/2.svg\"/>";
+            break;
+        case 3:
+            echo "<img src=\"../img/3.svg\"/>";
+            break;
+        case 4:
+            echo "<img src=\"../img/4.svg\"/>";
+            break;
+        case 5:
+            echo "<img src=\"../img/5.svg\"/>";
+            break;
+        case 6:
+            echo "<img src=\"../img/6.svg\"/>";
+            break;
+    }
+    
+    unset($listaDados[$dadoaEliminar]); // Eliminar el dado específico en la lista de dados anterior
+
+     echo nl2br("<h2>Dados restantes</h2>\n");
+     foreach ($listaDados as $dado) {
+        switch ($dado) {
+            case 1:
+                echo "<img src=\"../img/1.svg\"/>";
+                break;
+            case 2:
+                echo "<img src=\"../img/2.svg\"/>";
+                break;
+            case 3:
+                echo "<img src=\"../img/3.svg\"/>";
+                break;
+            case 4:
+                echo "<img src=\"../img/4.svg\"/>";
+                break;
+            case 5:
+                echo "<img src=\"../img/5.svg\"/>";
+                break;
+            case 6:
+                echo "<img src=\"../img/6.svg\"/>";
+                break;
+        }
+    }
+
+?>
